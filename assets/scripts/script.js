fa7f8547305e1b139e6ecb7bc9a3a248cf70c93d
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-18 21:10:07 -0700

    starting the dopamenu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,37 @@
 console.log("anything but hello world")
+
+// mood vs activity tabs 
+function changeTab(evt, type) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(type).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // different mood avtivity tabs
+  function moodTabs(evt, type) {
+    var i, tabcontentTwo, tablinks;
+  
+    tabcontentTwo = document.getElementsByClassName("tabcontentTwo");
+    for (i = 0; i < tabcontentTwo.length; i++) {
+        tabcontentTwo[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(type).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
