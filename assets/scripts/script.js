1dda89587d47e4141a4393aa27adbbab6e0e07ec
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-22 00:50:02 -0700

    moving activity js into a separate file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,59 +1,9 @@
 
-// mood vs activity tabs
-function changeTab(evt, type) {
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(type).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// different mood  category tabs
-function moodTabs(evt, type) {
-  var i, tabcontentTwo, tablinks;
-
-  tabcontentTwo = document.getElementsByClassName("tabcontentTwo");
-  for (i = 0; i < tabcontentTwo.length; i++) {
-    tabcontentTwo[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(type).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// different activity category tabs
-function activityTabs(evt, type) {
-  var i, tabcontentThree, tablinks;
-
-  tabcontentThree = document.getElementsByClassName("tabcontentThree");
-  for (i = 0; i < tabcontentThree.length; i++) {
-    tabcontentThree[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(type).style.display = "block";
-  evt.currentTarget.className += " active";
-}
 
 
 // toggling modal to show automatically
-let timesUpModal = new bootstrap.Modal(document.getElementById('timesup-modal'), {})
-timesUpModal.toggle()
+let timesUpModal = new bootstrap.Modal(
+  document.getElementById("timesup-modal"),
+  {}
+);
+timesUpModal.toggle();
