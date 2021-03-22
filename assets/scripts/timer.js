24901e4cdb0f12369375603741e84fba74009395
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-22 11:30:06 -0700

    adding event listeners for timer buttons

---
+++ assets/scripts/timer.js
@@ -0,0 +1,29 @@
+console.log("ehe")
+
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+
+let hour = document.getElementById("hours")
+let minute = document.getElementById("minutes")
+let second = document.getElementById("seconds")
+
+let startTimer = null;
+
+function decTime (){
+
+}
+
+start.addEventListener("click", function(){
+    console.log("plz")
+})
+
+
+reset.addEventListener("click", function(){
+    console.log("reset")
+})
+
+stop.addEventListener("click", function(){
+    console.log("stop")
+})
