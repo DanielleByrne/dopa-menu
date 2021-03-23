dd688a0e8c286d626bf174003a755c37ca940809
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-22 17:09:22 -0700

    fixiing dopa menu for millionth time

--- assets/scripts/activity.js
+++ assets/scripts/activity.js
@@ -1,7 +1,7 @@
 // //default open mood tab
-// document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
 
-// // mood vs activity tabs
+// mood vs activity tabs
 // function changeTab(evt, type) {
 //   var i, tabcontent, tablinks;
 
@@ -19,38 +19,38 @@
 //   evt.currentTarget.className += " active";
 // }
 
-// // different mood  category tabs
-// function moodTabs(evt, type) {
-//   var i, tabcontentTwo, tablinks;
+// different mood  category tabs
+function moodTabs(evt, type) {
+  var i, tabcontentTwo, tablinks;
 
-//   tabcontentTwo = document.getElementsByClassName("tabcontentTwo");
-//   for (i = 0; i < tabcontentTwo.length; i++) {
-//     tabcontentTwo[i].style.display = "none";
-//   }
+  tabcontentTwo = document.getElementsByClassName("tabcontentTwo");
+  for (i = 0; i < tabcontentTwo.length; i++) {
+    tabcontentTwo[i].style.display = "none";
+  }
 
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-//   document.getElementById(type).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-// // different activity category tabs
-// function activityTabs(evt, type) {
-//   var i, tabcontentThree, tablinks;
+// different activity category tabs
+function activityTabs(evt, type) {
+  var i, tabcontentThree, tablinks;
 
-//   tabcontentThree = document.getElementsByClassName("tabcontentThree");
-//   for (i = 0; i < tabcontentThree.length; i++) {
-//     tabcontentThree[i].style.display = "none";
-//   }
+  tabcontentThree = document.getElementsByClassName("tabcontentThree");
+  for (i = 0; i < tabcontentThree.length; i++) {
+    tabcontentThree[i].style.display = "none";
+  }
 
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-//   document.getElementById(type).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
+}
