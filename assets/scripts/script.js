b770d3f49cab18ed5d65d072e9256917c7fea168
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-24 23:50:59 -0700

    added a complete activity button to take them to the times up and kudos modals

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
 
 
 
-// // toggling modal to show automatically
-// let timesUpModal = new bootstrap.Modal(
-//   document.getElementById("timesup-modal"),
-//   {}
-// );
-// timesUpModal.toggle();
+// toggling modal to show automatically
+let timesUpModal = new bootstrap.Modal(
+  document.getElementById("timesup-modal"),
+  {}
+);
+timesUpModal.toggle();
